cc147753b1ca906bbf85947a6690eebe6a62b8d4
Ashish <ashking94@gmail.com>
2017-07-08 20:51:13 +0530

    Enabled CORS for APIs

--- src/main/java/com/zenefits/bizlayer/restapi/application/RestApiService.java
+++ src/main/java/com/zenefits/bizlayer/restapi/application/RestApiService.java
@@ -1,5 +1,11 @@
 package com.zenefits.bizlayer.restapi.application;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,6 +50,17 @@ public class RestApiService extends Application<RestApiConfig> {
 
 		LOGGER.info("Registering resources");
 		environment.jersey().register(new ThirdPartyResource());
+
+		// Enable CORS headers
+		final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		// Configure CORS parameters
+		cors.setInitParameter("allowedOrigins", "*");
+		cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+		// Add URL mapping
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
 	}
 
